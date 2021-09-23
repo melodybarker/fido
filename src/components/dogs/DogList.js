1cbbde1d643c7ad7d65542ea7c1808db6ca8efc4
Melody Barker <melodybarker2013@gmail.com>
2021-09-22 23:40:20 -0500

    complete most of message and filter

--- src/components/dogs/DogList.js
+++ src/components/dogs/DogList.js
@@ -1,142 +1,248 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DogContext } from "./DogProvider";
+import { UserContext } from "../users/UserProvider";
+import { MessageContext } from "../messages/MessageProvider";
 import { Link, useHistory } from "react-router-dom";
 import "./Dog.css";
 
 export const DogList = (props) => {
   const { dogs, getDogs } = useContext(DogContext);
   const [dog, setDogs] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [items, setItems] = useState([]);
-	const [show, setShow] = useState(false)
-  const history = useHistory();
-  const currentUserId = parseInt(localStorage.getItem("fido_user"));
+  // const { addMessages } = useContext(MessageContext);
+  const [users, setUsers] = useState([]);
+  const [messages, getMessages] = useState([]);
 
-		const showList = () => {
-			if (currentUserId === parseInt(dog.currentUserId)) {
-      setShow(!show)
-    }}
+  // filter function
+  const [searchParam] = useState(["breed"]);
+
+  const [searchDog, setSearchDog] = useState("");
+  const [filteredDogs, setFilteredDogs] = useState([]);
+
+  const [show, setShow] = useState(false);
+  const history = useHistory();
+  const userId = parseInt(localStorage.getItem("fido_user"));
+
+  // show/hide list for login
+  const showList = () => {
+    if (userId === parseInt(dog.userId)) {
+      setShow(!show);
+    }
+  };
 
   useEffect(() => {
     console.log("DogList: useEffect - getDogs");
     getDogs();
   }, []);
 
-  // filter function
-  const [searchParam] = useState(["breed"]);
-  const [filterParam, setFilterParam] = useState(["All"]);
-
   useEffect(() => {
-    fetch("http://localhost:8088/dogs")
-      .then((res) => res.json())
-      .then((result) => {
-        setIsLoading(true);
-        setItems(result);
-      });
-  });
+    setFilteredDogs(dogs);
+  }, [dogs]);
 
-  function search(items) {
-    return items.filter((item) => {
-      if (item.breed === filterParam) {
-        return searchParam.some((newItem) => {
-          return (
-            items[newItem]
-              .toString()
-              .toLowerCase()
-              .indexOf(dogs.toLowerCase()) > -1
-          );
-        });
-      }
-      if (filterParam === "All") {
-        return searchParam.som((newItem) => {
-          return (
-            item[newItem].toString().toLowerCase().indexOf(dogs.toLowerCase()) >
-            -1
-          );
-        });
-      }
-    });
+  const handleSearch = (e) => {
+    setSearchDog(e.target.value);
+  };
+  const handleBreed = (e) => {
+    let DogBreed = [...filteredDogs];
+    DogBreed = dogs.filter(
+      (dog) => dog.breed === e.target.value
+    );
+    setFilteredDogs(DogBreed);
+    FindDogs()
   }
+
+  const handleLocation = (e) => {
+  let DogLocation = [...filteredDogs];
+  DogLocation = dogs.filter(
+      (dog) => dog.location === e.target.value
+    );
+    setFilteredDogs(DogLocation)
+    FindDogs()
+  }
+  const handleGender = (e) => {
+    let DogGender = [...filteredDogs];
+    DogGender = dogs.filter(
+      (dog) => dog.gender === e.target.value
+    )
+    setFilteredDogs(DogGender)
+    FindDogs()
+  }
+  // filter function
+  const filterAllDogs = dog.filter(
+    (d) =>
+      d.breed.toLowerCase().includes(searchDog.toLowerCase()) &&
+      d.location.toLowerCase().includes(searchDog.toLowerCase()) &&
+      d.gender.toLowerCase().includes(searchDog.toLowerCase())
+  );
+  const FindDogs = () => {
+    // grab value of breed dropdown
+    // set an array that filters the choosen breed
+    // set that array to filteredDogs
+    return (
+      <div className="filterDogs">
+        {filterAllDogs.map((d) => (
+          <dog key={dog} id={dog} dog={dog} />
+        ))}
+      </div>
+    );
+  };
+
+  // setting the date to show MM/DD/YYYY
+  const month = new Date().getMonth() + 1;
+  const day = new Date().getDate();
+  const year = new Date().getFullYear();
+  const currentDate = month + "/" + day + "/" + year;
+
+  const cities = [
+    "Filter by Location",
+    "Ashland, TN",
+    "Asheville, TN",
+    "Brentwood, TN",
+    "Chattanooga, TN",
+    "Clarksville, TN",
+    "Cookville, TN",
+    "Crossville, TN",
+    "Dickson, TN",
+    "Dunlap, TN",
+    "Fairview, TN",
+    "Franklin, TN",
+    "Gallatin, TN",
+    "Gatlinburg, TN",
+    "Germantown, TN",
+    "Hendersonville, TN",
+    "Jackson, TN",
+    "Johnson, TN",
+    "Kingsport, TN",
+    "Knoxville, TN",
+    "La Vergne, TN",
+    "Lynchburg, TN",
+    "Maryville, TN",
+    "Memphis, TN",
+    "Morristown, TN",
+    "Mt Juliet, TN",
+    "Murfreesboro, TN",
+    "Nashville, TN",
+    "Nolensville, TN",
+    "Oak Ridge, TN",
+    "Ooltewah, TN",
+    "Paris, TN",
+    "Pigeon, TN",
+    "Rogersville, TN",
+    "Signal, TN",
+    "Smyrna, TN",
+    "Spring, TN",
+    "Tullahoma, TN",
+  ];
+  const breeds = [
+    "Filter by Breed",
+    "Unknown",
+    "Australian Shepard",
+    "Basset Hound",
+    "Beagle",
+    "Bernese Mountain Dog",
+    "Blue Heeler",
+    "Border Collie",
+    "Boston Terrier",
+    "Bulldog",
+    "Chihuahua",
+    "Chow Chow",
+    "Cocker Spaniel",
+    "Dachshund",
+    "Dalmatian",
+    "German Shepherd",
+    "Golden Retriever",
+    "Great Dane",
+    "Grey Hound",
+    "Husky",
+    "Irish Setter",
+    "Jack Russell Terrier",
+    "Maltese",
+    "Mix Breed",
+    "Pit Bull",
+    "Poodle",
+    "Pug",
+    "Rottweiler",
+    "Westie",
+  ];
+  const genders = [
+  "Filter by Gender",
+  "Female",
+  "Male"
+  ];
 
   return (
     <>
-		<fieldset className="DogListForm">
-      <select
-        onChange={(event) => {
-          setFilterParam(event.target.value);
-        }}
-        className="filter_breed"
-      >
-        <option value="All">Filter By Breed</option>
-        <option value="Unknown">Unknown</option>
-        <option value="Australian Shepard">Australian Shepard</option>
-        <option value="Basset Hound">Basset House</option>
-        <option value="Beagle">Beagle</option>
-        <option value="Bernese Mountain Dog">Bernese Mountain Dog</option>
-        <option value="Border Collie">Border Collie</option>
-        <option value="Boston Terrier">Boston Terrier</option>
-        <option value="Bulldog">Bulldoge</option>
-        <option value="Chihuahua">Chihuahua</option>
-        <option value="Chow Chow">Chow Chow</option>
-        <option value="Cocker Spaniel">Cocker Spaniel</option>
-        <option value="Dachshund">Dachshund</option>
-        <option value="Dalmatian">Dalmatian</option>
-        <option value="German Shepherd">German Shepherd</option>
-        <option value="Golden Retriever">Golden Retriever</option>
-        <option value="Great Dane">Great Dane</option>
-        <option value="Grey Hound">Grey Hound</option>
-        <option value="Husky">Husky</option>
-        <option value="Irish Setter">Irish Setter</option>
-        <option value="Jack Russell Terrier">Jack Russell Terrier</option>
-        <option value="Maltese">Maltese</option>
-        <option value="Mix Breed">Mix Breed</option>
-        <option value="Pit Bull">Pit Bull</option>
-        <option value="Poodle">Poodle</option>
-        <option value="Pug">Pug</option>
-        <option value="Rottweiler">Rottweiler</option>
-      </select>
-      <span className="focus"></span>
+      <fieldset className="DogFilter">
+        <div onChange={handleLocation} className="DogFilter" value={cities}>
+          <select id="DogLocation" onChange={handleLocation} className="DogFilter">
+            {cities.map((city) => {
+              return (
+                <>
+                  <option value={city}>{city}</option>
+                </>
+              );
+            })}
+          </select>
+        </div>
 
-      <h3 className="dogTitle">Search Missing Dogs</h3>
-      <div className="DogList">
-        {dogs.map((dog) => {
-          return (
-            <section className="dogPost" key={dog.id} id={`dog--${dog.id}`}>
-              <div className="dog_lost">{dog.lost}</div>
-              <img
-                className="dog_url"
-                src={dog.url}
-                width="300px"
-                height="350px"
-              />
-
-              <div className="dog_name">Name: {dog.name}</div>
-              <div className="dog_breed">Breed: {dog.breed}</div>
-              <div className="dog_gender">Gender: {dog.gender}</div>
-              <div className="dog_location">Location: {dog.location}</div>
-              <div className="dog_date">Date: {dog.date}</div>
-              <div className="dog_info">Info: {dog.info}</div>
-              <div className="currentUser">
-                message user:
-                <Link onClick={() => history.push("/dogs/message")}>
-                  {dog.currentUserId}
-                </Link>
-              </div>
-              <button onChange={showList} className="editDog"
-                onClick={() => {
-                  history.push(`/dogs/edit/${dog.id}`);
-                }}
-              >
-                Edit
-              </button>
-            </section>
-
-          );
-        })}
-				{show ? <><DogList/></> : <></>}
-      </div>
-			</fieldset>
+        <div onChange={handleBreed} className="DogFilter" value={breeds}>
+          <select id="DogBreed" onChange={handleBreed} className="DogFilter">
+            {breeds.map((breed) => {
+              return (
+                <>
+                  <option value={breed}>{breed}</option>
+                </>
+              );
+            })}
+          </select>
+        </div>
+          <div onChange={handleGender} className="DogGender" value={genders}>
+          <select id="DogGender" onChange={handleGender} className="DogFilter">
+            {genders.map((gender) => {
+              return <option value={gender}>{gender}</option>;
+            })}
+          </select>
+        </div>
+      </fieldset>
+      <fieldset className="DogForm">
+        <h3 className="dogTitle">Search Missing Dogs</h3>
+        <div className="DogList">
+          {filteredDogs.map((dog) => {
+            return (
+              <section className="dogPost" key={dog.id} id={`dog--${dog.id}`}>
+                <div className="dog_lost">{dog.lost}</div>
+                <img
+                  className="dog_url"
+                  src={dog.url}
+                  width="300px"
+                  height="350px"
+                />
+                <div className="dog_id">ID: {dog.id}</div>
+                <div className="dog_name">Name: {dog.name}</div>
+                <div className="dog_breed">Breed: {dog.breed}</div>
+                <div className="dog_gender">Gender: {dog.gender}</div>
+                <div className="dog_location">Location: {dog.location}</div>
+                <div className="dog_date">Date: {dog.date}</div>
+                <div className="dog_info">Info: {dog.info}</div>
+                <div className="currentUser">
+                  message user:
+                  <Link onClick={() => history.push("/messages/form")}>
+                    {dog.user.name}
+                  </Link>
+                </div>
+                <button
+                  onChange={showList}
+                  className="editDog"
+                  onClick={() => {
+                    history.push(`/dogs/edit/${dog.id}`);
+                  }}
+                >
+                  Edit
+                </button>
+              </section>
+            );
+          })}
+        </div>
+      </fieldset>
     </>
   );
 };
-
--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,41 +1,48 @@
-import React, {useContext, useEffect} from "react"
-import {MessageContext} from "./MessageProvider"
-import {useHistory} from "react-router-dom"
-import "./Message.css"
+import React, { useContext, useEffect } from "react";
+import { MessageContext } from "./MessageProvider";
+import { useHistory, useParams } from "react-router-dom";
+import "./Message.css";
 
 export const MessageList = () => {
-
-  const history = useHistory()
-  const {messages, getMessages} = useContext(MessageContext)
+  const history = useHistory();
+  const { messages, getMessages } = useContext(MessageContext);
 
   useEffect(() => {
-    console.log("MessageList: useEffect - getMessages")
-    getMessages()
-  }, [])
+    console.log("MessageList: useEffect - getMessages");
+    getMessages();
+  }, []);
 
   return (
     <>
-    <h3>Messages</h3>
-    <button onClick={() => history.push("/messages/send")}>
-      Send A Message
-    </button>
-    <div className="messagesReceived">
-      {messages.map((message) => {
-        return (
-          <div className="message" key={message.id} id={`message--${message.id}`}>
-          <div className="messageRecipient">
-            <b>Recipient: </b>{message.user.name}
-          </div>
-          <div className="messageBody">
-            <i>{message.message}</i>
-          </div>
-          <div className="messageDate">
-            <b>Date Sent: </b>{message.sendDate}
-          </div>
-          </div>
-        )
-      })}
-    </div>
+      <section className="MessageForm">
+        <label className="MessageTitle">Messages</label>
+        <button onClick={() => history.push("/messages/form")}>
+          Send A Message
+        </button>
+        <div className="messagesReceived">
+          {messages.map((message) => {
+            return (
+              <div
+                className="message"
+                key={message.id}
+                id={`message--${message.id}`}
+              >
+                <div className="messageRecipient">
+                  <b>Recipient: </b>
+                  {message.userId}
+                </div>
+                <div className="messageBody">
+                  <i>{message.message}</i>
+                </div>
+                <div className="messageDate">
+                  <b>Date Sent: </b>
+                  {message.sendDate}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
